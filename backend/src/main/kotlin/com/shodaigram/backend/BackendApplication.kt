31afcaf06add3cb5495a66f6c0d097e08941b7e2
T-Angeleski <teodorangeleski@gmail.com>
2025-10-18 20:42:16 +0200

    ci: CI configuration and improve code quality checks

--- backend/src/main/kotlin/com/shodaigram/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/shodaigram/backend/BackendApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class BackendApplication
 
+@Suppress("SpreadOperator")
 fun main(args: Array<String>) {
     runApplication<BackendApplication>(*args)
-    println("Hello")
 }
